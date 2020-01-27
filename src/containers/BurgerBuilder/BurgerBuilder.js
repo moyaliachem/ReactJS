8b70aef4125260126e514eb0ee2721f3e974a8e2
Jean Liachem Moya <jmoya@stratpoint.com>
2020-01-27 15:36:06 +0800

    asd

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Aux from '../../hoc/Auxillary';
+import Burger from '../../components/Burger/Burger';
+
+class BurgerBuilder extends Component {
+    render(){
+        return(
+            <Aux>
+                <Burger/>
+                <div>Build Controls</div>
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
